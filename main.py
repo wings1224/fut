5f3249ce5f184f7b34b421e7352b2b5181d4c08a
wings <wings1224@gmail.com>
2023-08-25 18:51:37 +0800

    1

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     api = ApiInterface("https://utas.mob.v1.fut.ea.com/ut/", config.G_SID)
     user = User(config.G_SID, api)
 
-    # user.aotobuypack(pack_id=101, times=50)
+    user.aotobuypack(pack_id=101, times=50)
     
     # sbc and open pack 
     # broze 3054
